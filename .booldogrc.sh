1a0b09f538bdc0ddad74e86ebf3d95371ccf7e21
olecsys <olecsys@gmail.com>
2019-08-06 11:25:08 +0400

    add .booldogrc.sh

---
+++ .booldogrc.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function main() {
+  local old_dir="`pwd`/`basename "$0"`"
+  old_dir=`dirname "$old_dir"`
+  cd "`dirname "$0"`"
+  local script_dir="`pwd`/`basename "$0"`"
+  script_dir=`dirname "$script_dir"`
+  cd "$old_dir"
+
+  local red='\e[0;31m'
+  local green='\e[0;32m'
+  local nocolor='\e[0m'
+  local redbold='\e[1;31m'
+  local greenbold='\e[1;32m'
+  local nocolorbold='\e[1m'
+  
+  local __funcerror=     # set `__funcerror` variable with custom error message 
+  local __funcresult=1   # set `__funcresult` variable with script result
+
+  while true
+  do
+    alias iv7echo='echo "IV7 RULEZZZZ"' || break
+
+    __funcresult=0
+    break
+  done  
+  
+  cd "$old_dir"
+
+  while true
+  do
+    [ ! -z "$__funcerror" ] \
+        && { echo -e "
+${red}${__funcerror}${nocolor}
+" 1>&2 ; __funcresult=1 ; break; }
+    
+    [ $__funcresult -eq 0 ] \
+        && { echo -e '
+'${greenbold}'.booldogrc.sh'${green}' initialization is succeeded
+'${nocolor} ; break; }
+        
+    echo -e '
+'${redbold}'.booldogrc.sh'${red}' initialization is failed
+'${nocolor} 1>&2
+    
+    break
+  done
+}
+main $@
